afb82cb35c8d3acd9650636c0d0ee55631dce62f
Tanishq Tiwari <tanishqtiwari2020@gmail.com>
2025-08-24 06:44:35 +0530

    feat: Add scroll animations and metallic logo effect
    - Introduced a new HeroScrollDemo component for showcasing scroll animations.
    - Added a new logo component that utilizes a metallic paint effect with an SVG logo.
    - Implemented a ContainerScroll component to handle scroll-based animations.
    - Created a utility function for class name merging.
    - Added a new SVG asset for the logo.

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,35 @@ import ParentL from './pages/LoginPage/Parent/ParentL';
 import EnterpriseR from './pages/RegisterPage/Enterprise/EnterpriseR';
 import ParentR from './pages/RegisterPage/Parent/ParentR';
 import LandingPage from './pages/LandingPage/LandingPage';
+import Logo from './components/logo/Logo';
+import { div } from 'framer-motion/client';
+import { HeroScrollDemo } from './components/HeroScrollDemo';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/landing_page/*" element={<LandingPage />} />
-        <Route path="/login/parent/*" element={<ParentL />} />
-        <Route path="/login/enterprise/*" element={<EnterpriseL />} />
-        <Route path="/register/parent/*" element={<ParentR />} />
-        <Route path="/register/enterprise/*" element={<EnterpriseR />} />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/landing_page/*" element={<LandingPage />} />
+    //     <Route path="/login/parent/*" element={<ParentL />} />
+    //     <Route path="/login/enterprise/*" element={<EnterpriseL />} />
+    //     <Route path="/register/parent/*" element={<ParentR />} />
+    //     <Route path="/register/enterprise/*" element={<EnterpriseR />} />
 
-        <Route
-          path="/"
-          element={<Navigate to="/landing_page" replace />}
-        />
-        <Route
-          path="*"
-          element={<Navigate to="/landing_page" replace />}
-        />
-      </Routes>
-    </BrowserRouter>
+    //     <Route
+    //       path="/"
+    //       element={<Navigate to="/landing_page" replace />}
+    //     />
+    //     <Route
+    //       path="*"
+    //       element={<Navigate to="/landing_page" replace />}
+    //     />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <HeroScrollDemo />
+    </div>
+
   );
 }
 
