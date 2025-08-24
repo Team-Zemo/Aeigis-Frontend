4380bade1c3fe65537ce738685396c6fc2cdaf04
Tanishq Tiwari <tanishqtiwari2020@gmail.com>
2025-08-24 07:01:08 +0530

    feat: Implement MagicBento component with particle effects and spotlight feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import EnterpriseL from './pages/LoginPage/Enterprise/EnterpriseL';
-import ParentL from './pages/LoginPage/Parent/ParentL';
-import EnterpriseR from './pages/RegisterPage/Enterprise/EnterpriseR';
-import ParentR from './pages/RegisterPage/Parent/ParentR';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Logo from './components/logo/Logo';
-import { div } from 'framer-motion/client';
-import { HeroScrollDemo } from './components/HeroScrollDemo';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import EnterpriseL from "./pages/LoginPage/Enterprise/EnterpriseL";
+import ParentL from "./pages/LoginPage/Parent/ParentL";
+import EnterpriseR from "./pages/RegisterPage/Enterprise/EnterpriseR";
+import ParentR from "./pages/RegisterPage/Parent/ParentR";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Logo from "./components/logo/Logo";
+import { div } from "framer-motion/client";
+import { HeroScrollDemo } from "./components/HeroScrollDemo";
+import MagicBento  from "./components/MagicGrid";
 
 function App() {
-
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/landing_page/*" element={<LandingPage />} />
-    //     <Route path="/login/parent/*" element={<ParentL />} />
-    //     <Route path="/login/enterprise/*" element={<EnterpriseL />} />
-    //     <Route path="/register/parent/*" element={<ParentR />} />
-    //     <Route path="/register/enterprise/*" element={<EnterpriseR />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/landing_page/*" element={<LandingPage />} />
+        <Route path="/login/parent/*" element={<ParentL />} />
+        <Route path="/login/enterprise/*" element={<EnterpriseL />} />
+        <Route path="/register/parent/*" element={<ParentR />} />
+        <Route path="/register/enterprise/*" element={<EnterpriseR />} />
 
-    //     <Route
-    //       path="/"
-    //       element={<Navigate to="/landing_page" replace />}
-    //     />
-    //     <Route
-    //       path="*"
-    //       element={<Navigate to="/landing_page" replace />}
-    //     />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>
-      <HeroScrollDemo />
-    </div>
-
+        <Route
+          path="/"
+          element={<Navigate to="/landing_page" replace />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/landing_page" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
