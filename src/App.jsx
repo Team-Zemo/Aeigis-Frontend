5e2ffea453ad18bfecbd8dd582a47f9683af30c2
Yadveer1 <134469422+Yadveer1@users.noreply.github.com>
2025-08-24 15:51:25 +0530

    refactor: Clean up App component structure and remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import ParentL from './pages/LoginPage/Parent/ParentL';
 import EnterpriseR from './pages/RegisterPage/Enterprise/EnterpriseR';
 import ParentR from './pages/RegisterPage/Parent/ParentR';
 import LandingPage from './pages/LandingPage/LandingPage';
-import DashBoard from './pages/DashBoard/DashBoard';
 import Logo from './components/logo/Logo';
 import Enterprise from './pages/Dashboard/Enterprise';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
@@ -27,13 +26,25 @@ const theme = createTheme({
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home/*" element={<LandingPage />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </BrowserRouter>
-
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/home/*" element={<LandingPage />} />
+          <Route path="/enterprise/login" element={<EnterpriseL />} />
+          <Route 
+            path="/enterprise/dashboard" 
+            element={
+              <ProtectedRoute>
+                <Enterprise />
+                {/* <Dashboard /> */}
+              </ProtectedRoute> 
+            } 
+          />
+          <Route path="*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
