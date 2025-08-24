6f8bee503e4ff4094fa42180fed35cbc3f8d430a
Tanishq Tiwari <tanishqtiwari2020@gmail.com>
2025-08-24 12:35:19 +0530

    feat: Add DashBoard and Setting components, implement AI misuse detection features, and update ChartRenderer for enhanced data handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import ParentL from './pages/LoginPage/Parent/ParentL';
 import EnterpriseR from './pages/RegisterPage/Enterprise/EnterpriseR';
 import ParentR from './pages/RegisterPage/Parent/ParentR';
 import LandingPage from './pages/LandingPage/LandingPage';
+import DashBoard from './pages/DashBoard/DashBoard';
 import Logo from './components/logo/Logo';
+import Setting from "./pages/ChatPage/Setting.jsx";
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home/*" element={<LandingPage />} />
-        <Route path="*" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </BrowserRouter>
-
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/home/*" element={<LandingPage />} />
+    //     <Route path="/dashboard" element={<DashBoard />} />
+    //     <Route path="*" element={<Navigate to="/home" replace />} />
+    //   </Routes>
+    // </BrowserRouter>
+<Setting/>
   );
 }
 
