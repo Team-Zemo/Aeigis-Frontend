1b361de89faa7a6ccd99c5e4ad03bea0ea067fb2
Yadveer1 <134469422+Yadveer1@users.noreply.github.com>
2025-08-24 15:49:33 +0530

    feat: Create Dashboard component with SidebarDemo and Enterprise integration

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import SidebarDemo from "../../components/SidebarDemo"
+import Enterprise from "./Enterprise"
+
+function Dashboard() {
+    return (
+        <>
+            <SidebarDemo />
+            <Enterprise />
+        </>
+    )
+}
+export default Dashboard;
